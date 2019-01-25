06ba5731e3b64bfc2dcaebbdb82c8a0038e44684
gabormay <gabor@maylander.net>
2019-01-25 14:31:13 +0100

    Updates

---
+++ precache-manifest.18c30dc40728db21bb60107fee53507d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "5357e41e9c1b8587855a",
+    "url": "/static/css/main.d7b7417a.chunk.css"
+  },
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "/static/media/open-iconic.eca2c267.svg"
+  },
+  {
+    "revision": "0b1c2f03c85982bd27dd",
+    "url": "/static/js/1.0b1c2f03.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5357e41e9c1b8587855a",
+    "url": "/static/js/main.5357e41e.chunk.js"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "0b1c2f03c85982bd27dd",
+    "url": "/static/css/1.ca810b48.chunk.css"
+  },
+  {
+    "revision": "8a580173fb17d439f30cd63b3f7f667c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/www.maylander.net/precache-manifest.62ddc44cf9dc8c169874e37f545462c9.js"
+  "/precache-manifest.18c30dc40728db21bb60107fee53507d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/www.maylander.net/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
