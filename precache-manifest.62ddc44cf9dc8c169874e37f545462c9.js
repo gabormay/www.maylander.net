5876a42c7815db599d6e4f7d5fc9ca218cdd332b
gabormay <gabor@maylander.net>
2019-01-25 14:11:53 +0100

    Updates

---
+++ precache-manifest.62ddc44cf9dc8c169874e37f545462c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/www.maylander.net/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "6f33b76af6aa07f8468e",
+    "url": "/www.maylander.net/static/css/main.d7b7417a.chunk.css"
+  },
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "/www.maylander.net/static/media/open-iconic.eca2c267.svg"
+  },
+  {
+    "revision": "3597c931c2babe765072",
+    "url": "/www.maylander.net/static/js/1.3597c931.chunk.js"
+  },
+  {
+    "revision": "392ee040d0ed845f26ee",
+    "url": "/www.maylander.net/static/js/runtime~main.392ee040.js"
+  },
+  {
+    "revision": "6f33b76af6aa07f8468e",
+    "url": "/www.maylander.net/static/js/main.6f33b76a.chunk.js"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/www.maylander.net/static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/www.maylander.net/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/www.maylander.net/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "3597c931c2babe765072",
+    "url": "/www.maylander.net/static/css/1.ce4dccd6.chunk.css"
+  },
+  {
+    "revision": "58dc0bb830e4b45fa4f2ad6cc1e54cf2",
+    "url": "/www.maylander.net/index.html"
+  }
+];
